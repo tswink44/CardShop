f436c779c351be6b568077b4f33b5f6491c708ab
tswink44 <143281048+tswink44@users.noreply.github.com>
2024-11-12 19:51:28 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000", // Replace with your FastAPI backend URL
+    baseURL: "http://localhost:8000", 
 });
 
 export const getCards = async () => {
